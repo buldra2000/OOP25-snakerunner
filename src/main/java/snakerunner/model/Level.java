be7dade039154ee1de8843512cdd3314bd70e2dc
Galli Mattia <mattia.galli9@studio.unibo.it>
2026-01-20 16:07:00 +0100

    Added interface of the level

--- src/main/java/snakerunner/model/Level.java
+++ src/main/java/snakerunner/model/Level.java
@@ -1,5 +1,6 @@
 package snakerunner.model;
 
+<<<<<<< HEAD
 import java.util.Set;
 import snakerunner.commons.Point2D;
 
@@ -9,4 +10,7 @@ public interface Level {
     public Set<Point2D<Integer, Integer>> getObstacles();
 
     public  boolean IsBlocked(Point2D<Integer, Integer> position);
+=======
+public interface Level {
+>>>>>>> 9831ab0 (Added interface of the level)
 }
