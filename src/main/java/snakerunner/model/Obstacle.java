cf9388497c53baa5770f50903df5132be1a59fe9
Betul Ince <betul@192.168.1.27>
2026-01-30 22:46:42 +0100

    changed obstacle. Considering adding rectangle control instead of every square

--- src/main/java/snakerunner/model/Obstacle.java
+++ src/main/java/snakerunner/model/Obstacle.java
@@ -1,8 +1,14 @@
 package snakerunner.model;
 
+import java.awt.Rectangle; //Maybe we can use another method. Contains all of the classes for creating user interfaces and for painting graphics and images.
+
 public class Obstacle extends Entity{
     
-    //Obstacle's position
+//Temporary dimensions
+    private int width= 50;
+    private int height= 50;
+
+    //Obstacle's inital position
     public Obstacle(int x, int y){
         this.x=x;
         this.y=y;
@@ -14,8 +20,9 @@ public class Obstacle extends Entity{
     }
 
 //We check if the obstacle is hit by the snake
-    public boolean isHit(int snakeX, int snakeY){
+    public boolean isHit(int snakeX, int snakeY, int snakeWidth, int snakeHeight){
         //If obstacle's and snake's coordinates are the same then this means that there is a collision.
+        //Probably it makes more sense to create rectangles instead of using exact coordinates.
  if (this.x==snakeX && this.y==snakeY){
     return true;
  }
